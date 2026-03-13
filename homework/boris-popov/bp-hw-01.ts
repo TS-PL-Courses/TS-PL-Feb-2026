83188ba463004b7bef9107f1545239d8625a7934
bpopov1914 <borisbpopov@gmail.com>
2026-03-13 14:56:55 +0200

    Part6-Exercise15-QAEstimate

--- homework/boris-popov/bp-hw-01.ts
+++ homework/boris-popov/bp-hw-01.ts
@@ -128,10 +128,14 @@ printId(569365);
 printId('MF56A45G');
 
 // Exercise 15: Write code calculating the ammount of time a QA engineer needs to test a number of functionalities
-// Step 1: Declare a variables: QA name (string), number of functionalities (whole number), lets accpet time needed to test one functionality is 2 hours.
-// Step 2: Calculate the total time needed to test all functionalities.
-// Step 3: Log the QA name, number of functionalities and the total time needed to test all functionalities using string interpolation like:
-// "The QA {QA name} will need {total time needed} hours to complete {number of functionalities} project/s."
+console.log('--- Exercise 15: Time calculation for QA testing ---');
+let qaName: string = 'Serius Black';
+let numberOfFunctionalities: number = 8;
+let timePerFunctionality: number = 2;
+let totalTime: number = numberOfFunctionalities * timePerFunctionality;
+console.log(
+  `The QA ${qaName} will need ${totalTime} hours to complete ${numberOfFunctionalities} task/s.`,
+);
 
 // Exercise 16: Union in objects
 // Step 1: Create a type for a user object.
