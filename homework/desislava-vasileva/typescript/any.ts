060ee5a5b12e8d063be2eb7b35e90200a760f848
Esa <desy.bukareva@gmail.com>
2026-03-16 21:59:50 +0200

    Experiemtns with typescript

---
+++ homework/desislava-vasileva/typescript/any.ts
@@ -0,0 +1,31 @@
+// npx ts-node typescript/special-types/any.ts
+
+// any is a type that disables type checking and effectively allows all types to be used.
+
+// example without any gives error:
+// let booleanValue = true;
+let booleanVariable: boolean = true;
+// booleanVariable = "string"; // Error: Type 'string' is not assignable to type 'boolean'.
+// Math.round(booleanVariable); // Error: Argument of type 'boolean' is not assignable to parameter of type 'number'.
+console.log(typeof booleanVariable); // boolean - no error
+
+// example with any - no error
+let anyVariable: any = true;
+console.log(typeof anyVariable);
+console.log(anyVariable);
+
+
+anyVariable = "string"; // string - no error as it can be "any" type
+console.log(anyVariable);
+
+anyVariable = 10; // number - no error as it can be "any" type
+console.log(anyVariable);
+
+anyVariable = [10, 20, 30]; // array - no error as it can be "any" type
+console.log(typeof anyVariable);
+console.log(anyVariable);
+
+anyVariable = 3.14;
+Math.round(anyVariable); // used as a number - no error as it can be "any" type
+console.log(anyVariable);
+console.log(typeof anyVariable)
