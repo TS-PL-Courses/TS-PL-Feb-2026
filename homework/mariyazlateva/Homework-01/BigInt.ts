064d431d0d1c9750dc7ab3251afa69860a127956
mimilota <mimilotaz@gmail.com>
2026-03-18 19:35:44 +0200

    Added homework (#81)
    Co-authored-by: Ivelina Kostadinova <ivelina.st.kostadinova@gmail.com>

---
+++ homework/mariyazlateva/Homework-01/BigInt.ts
@@ -0,0 +1,13 @@
+// npx ts-node typescript/Homework-01/Bigint.ts
+// Part 4 — BigInt (bigint)
+
+// Exercise 11: BigInt addition
+// Step 1: Create two bigint numbers.
+// Step 2: Add the two bigint numbers together.
+// Step 3: Log the result.
+let x: bigint = 123465478n;
+let y: bigint = 123n;
+let suma: bigint = x+y;
+console.log("x=", x);
+console.log("y=", y);
+console.log("sum", suma);
