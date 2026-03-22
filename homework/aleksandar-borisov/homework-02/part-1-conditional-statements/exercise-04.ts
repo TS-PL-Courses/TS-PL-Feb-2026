4369e1008fc094eb9ef635a917b3ae5b0711dae4
alexbborisov <aleksbborisov@gmail.com>
2026-03-22 13:50:35 +0200

    Resolved reviewed comments

--- homework/aleksandar-borisov/homework-02/part-1-conditional-statements/exercise-04.ts
+++ homework/aleksandar-borisov/homework-02/part-1-conditional-statements/exercise-04.ts
@@ -3,5 +3,5 @@ const text: string = 'alabalabala';
 if (text.length > 10) {
   console.log('The string has more than 10 characters');
 } else {
-  console.log('The string has less thank 10 characters');
+  console.log('The string has 10 or fewer characters');
 }
--- homework/aleksandar-borisov/homework-02/part-1-conditional-statements/exercise-06.ts
+++ homework/aleksandar-borisov/homework-02/part-1-conditional-statements/exercise-06.ts
@@ -2,7 +2,7 @@ const age: number = 17;
 const lapTime: number = 1.18;
 
 console.log(`Client is ${age} years old`);
-console.log(`Clients best lap time is ${lapTime}`);
+console.log(`Client's best lap time is ${lapTime}`);
 
 if (age >= 18 && age < 65 && lapTime < 1) {
   console.log('Client is allowed to compete');
