389d040ef9868de2363622531e42a77248f4e5a1
alexbborisov <aleksbborisov@gmail.com>
2026-03-22 10:13:33 +0200

    homework-02 aleksandar borisov

---
+++ homework/aleksandar-borisov/homework-02/part-1-conditional-statements/exercise-06.ts
@@ -0,0 +1,11 @@
+const age: number = 17;
+const lapTime: number = 1.18;
+
+console.log(`Client is ${age} years old`);
+console.log(`Clients best lap time is ${lapTime}`);
+
+if (age >= 18 && age < 65 && lapTime < 1) {
+  console.log('Client is allowed to compete');
+} else {
+  console.log('Client is NOT allowed to compete');
+}
