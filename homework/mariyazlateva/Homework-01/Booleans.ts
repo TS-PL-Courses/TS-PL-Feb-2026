9d7d05b4c840d8bbfe525a4606b678b2a47dbeff
Mariya Zlateva <mimilotaz@gmail.com>
2026-03-24 22:50:56 +0200

    Format code (Prettier) – quote style update

--- homework/mariyazlateva/Homework-01/Booleans.ts
+++ homework/mariyazlateva/Homework-01/Booleans.ts
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // npx ts-node Booleans.ts
-=======
-// npx ts-node typescript/Homework-01/Booleans.ts
->>>>>>> Added homework
-=======
-// npx ts-node Booleans.ts
->>>>>>> Comments fixed hopefully
 // Exercise 6
 
 let isLoggedIn: boolean = false;
 
 if (isLoggedIn) {
-<<<<<<< HEAD
   console.log('Welcome!');
 } else {
   console.log('Please log in');
-=======
-  console.log("Welcome!");
-} else {
-  console.log("Please log in");
->>>>>>> Added homework
 }
 
 // Exercise 7
@@ -29,8 +15,4 @@ let isAdult: boolean = true;
 
 let canEnter: boolean = hasTicket && isAdult;
 
-<<<<<<< HEAD
 console.log('Can enter:', canEnter);
-=======
-console.log("Can enter:", canEnter);
->>>>>>> Added homework
