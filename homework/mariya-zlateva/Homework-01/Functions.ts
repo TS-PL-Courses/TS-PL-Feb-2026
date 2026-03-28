351ef89ea48cedba36f1bae8ade1a07d727cadc2
mimilota <mimilotaz@gmail.com>
2026-03-28 18:03:48 +0200

    Homework 02 maria zlateva (#124)
    * Homework-02
    
    * Format code (Prettier) – quote style update
    
    * homework -02 fixes
    
    * fixed homework 02

--- homework/mariya-zlateva/Homework-01/Functions.ts
+++ homework/mariya-zlateva/Homework-01/Functions.ts
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 //npx ts-node Functions.ts
-=======
-//npx ts-node typescript/Homework-01/Functions.ts
->>>>>>> Added homework
-=======
-//npx ts-node Functions.ts
->>>>>>> Comments fixed hopefully
 // Part 6 — Functions (function)
 
 // Exercise 14: Function with union type
@@ -15,14 +7,10 @@
 // Step 3: Inside the function, log the id value.
 
 function printId(id: string | number): void {
-<<<<<<< HEAD
   console.log('ID value:', id);
-=======
-    console.log("ID value:", id);
->>>>>>> Added homework
 }
 printId(123);
-printId("abc");
+printId('abc');
 
 // Exercise 15: Write code calculating the amount of time a QA engineer needs to test a number of functionalities
 // Step 1: Declare a variables: QA name (string), number of functionalities (whole number), let's accept time needed to test one functionality is 2 hours.
@@ -30,21 +18,12 @@ printId("abc");
 // Step 3: Log the QA name, number of functionalities and the total time needed to test all functionalities using string interpolation like:
 // "The QA {QA name} will need {total time needed} hours to complete {number of functionalities} project/s."
 
-<<<<<<< HEAD
 let qaName: string = 'Mariya';
 let functionalitiesCount: number = 6;
 const hoursPerFunctionality: number = 2;
 let totalHours: number = functionalitiesCount * hoursPerFunctionality;
 console.log(
   `The QA ${qaName} will need ${totalHours} hours to complete ${functionalitiesCount} project/s.`,
-=======
-let qaName: string = "Mariya";         
-let functionalitiesCount: number = 6;  
-const hoursPerFunctionality: number = 2; 
-let totalHours: number = functionalitiesCount * hoursPerFunctionality;
-console.log(
-  `The QA ${qaName} will need ${totalHours} hours to complete ${functionalitiesCount} project/s.`
->>>>>>> Added homework
 );
 // Exercise 16: Union in objects
 // Step 1: Create a type for a user object.
@@ -59,7 +38,6 @@ type User = {
 };
 
 const user1: User = {
-<<<<<<< HEAD
   id: 1, // number
   name: 'IVan',
 };
@@ -69,14 +47,3 @@ const user2: User = {
 };
 console.log('User 1:', user1);
 console.log('User 2:', user2);
-=======
-  id: 1,        // number
-  name: "IVan",
-};
-const user2: User = {
-    id: "index1",
-    name: "Goro",
-}
-console.log("User 1:", user1);
-console.log("User 2:", user2)
->>>>>>> Added homework
