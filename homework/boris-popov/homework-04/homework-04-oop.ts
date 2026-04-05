66dcd6a48dfa20767a9732523e8647eea3ad76c5
bpopov1914 <borisbpopov@gmail.com>
2026-04-05 13:33:05 +0300

    Pt1-Ex03-CarRentalService

--- homework/boris-popov/homework-04/homework-04-oop.ts
+++ homework/boris-popov/homework-04/homework-04-oop.ts
@@ -46,14 +46,39 @@ const user1: User = {
 console.log(user1);
 
 // Exercise 3 - Car Rental Service (Encapsulation)
-// Step 1: Define a class named "CarRental" with properties: carType, costPerDay, and methods setCostPerDay, getCostPerDay, rentCar, and a constructor that sets the two properties.
-// Step 2: carType should be visible outside the class, but costPerDay should be private.
-//         All methods should be visible outside the class.
-//         The value of the carType property should not be changeable.
-// Step 3: Create three objects of the class CarRental with different car types and costs, for example: Hatchback, Sedan, SUV.
-// Step 4: Print the car rental details in the console by calling the method rentCar() for each object with a different number of days rented.
-// Example output: "Renting a Hatchback for 3 days will cost: $150"
-// Step 5: Change the costPerDay for one of the car types and print the new rental details in the console.
+console.log('-----Exercise 3 - Car Rental Service (Encapsulation)-----');
+class CarRental {
+  public readonly carType: string;
+  private costPerDay: number;
+
+  constructor(carType: string, costPerDay: number) {
+    this.carType = carType;
+    this.costPerDay = costPerDay;
+  }
+
+  public setCostPerDay(newCost: number): void {
+    this.costPerDay = newCost;
+  }
+
+  public getCostPerDay(): number {
+    return this.costPerDay;
+  }
+
+  public rentCar(days: number): void {
+    const totalCost = this.costPerDay * days;
+    console.log(`Renting a ${this.carType} for ${days} days will cost: $${totalCost}`);
+  }
+}
+
+const car1 = new CarRental('Hatchback', 50);
+const car2 = new CarRental('Sedan', 70);
+const car3 = new CarRental('SUV', 100);
+
+car1.rentCar(3);
+car2.rentCar(2);
+car3.rentCar(5);
+car1.setCostPerDay(60);
+car1.rentCar(3);
 
 // Exercise 4 - Furniture Factory (Inheritance)
 // Step 1: Create a class called "Chair" that inherits from the base class "Furniture".
