320e0abcaa6d5871d2d0cc15894951d7d4430ea9
VladimirChernev <vchernev@abv.bg>
2026-04-16 18:49:25 +0300

    formating updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests/',
+  testDir: './',
 
   // Give failing tests 3 retry attempts
   // retries: 3,
