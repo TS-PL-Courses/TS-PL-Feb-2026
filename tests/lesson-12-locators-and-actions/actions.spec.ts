68c6084e2afe638a2718d55ed0bf4dfab13e2b72
VladimirChernev <vchernev@abv.bg>
2026-04-16 16:52:37 +0300

    lecture 12 set up

---
+++ tests/lesson-12-locators-and-actions/actions.spec.ts
@@ -0,0 +1,8 @@
+import { test } from '@playwright/test';
+import path from 'path';
+
+// documentation: https://playwright.dev/docs/input
+
+test('Actions Examples', async ({ page }) => {
+
+});
---
+++ tests/lesson-12-locators-and-actions/locators.spec.ts
@@ -0,0 +1,9 @@
+import { test } from '@playwright/test';
+
+// documentation: https://playwright.dev/docs/locators
+// best practices: https://playwright.dev/docs/best-practices#best-practices
+
+test('Locator Examples', async ({ page }) => {
+  // https://st2016.inv.bg/login
+
+});
