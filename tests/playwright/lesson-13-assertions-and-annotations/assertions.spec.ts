3f76816b006a8b905ad44e58ee1d5eff847d315f
VladimirChernev <vchernev@abv.bg>
2026-04-21 17:13:34 +0300

    lecture 13 set up initial

---
+++ tests/playwright/lesson-13-assertions-and-annotations/assertions.spec.ts
@@ -0,0 +1,4 @@
+import { test, expect } from '@playwright/test';
+
+// original jest library documentation: https://jestjs.io/docs/expect
+// playwright documentation: https://playwright.dev/docs/test-assertions
