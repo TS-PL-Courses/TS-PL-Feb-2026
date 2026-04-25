2d1d0ac2218141d17139ec64aa24713048d74304
VladimirChernev <vchernev@abv.bg>
2026-04-25 21:12:21 +0300

    homework 6

---
+++ homework/_assignments/homework-06.spec.ts
@@ -0,0 +1,55 @@
+// create the below 2 test scenarios
+// put all groups of actions into a test.step
+// use soft assertions, unless we are assrting a navigation has taken place
+// try to add meaningful comments to all assertions
+// you can add -ui mode debug your tests during execution
+// after execution view html report with command: 'npx playwright show-report' (if any tests failed report will open automaticaly)
+// a trace artifact will be added to the report if any test has failed, you can use as well to debug your tests
+
+
+// first test scenario use CSS and XPATH selectors like page.locator('location string'):
+
+// navigate to https://rahulshettyacademy.com/AutomationPractice/
+// assert succesful naviation
+
+// check radio button #2
+// assert Radio button #2 is checked
+
+// fill the input with "Bulgaria"
+// click on the first auto complete suggestion
+// Verify the input value is "Bulgaria"
+
+// select option #2 from the dropdown
+// Verify the selected option is "option2"
+
+// check checkbox #3
+// Verify checkbox #3 is checked
+
+// click on the "Hide" button
+// Verify the input element is now hidden
+
+// hover over the "Mouse Hover" element
+// Verify the "Top" link is now visible
+
+
+// second test scenario use only Playwright selectors like page.getByRole('button', { name: 'Вход' }).click();
+
+// navigate to https://testautomationpractice.blogspot.com/p/playwrightpractice.html
+// assert succesful naviation
+
+// In Playwright Practice session group 1. getByRole() Locators pick a locator from section Navigation and click on it.
+// assert navigation has taken place (url has changed)
+
+// In Playwright Practice session group 2. getByText() Locators pick one of the paragraphs with colored text and assert is color
+
+// In Playwright Practice session group 3. getByLabel() Locators Locators click the unchecked radio button
+// assert is is checked now
+
+// In Playwright Practice session group 4. getByPlaceholder() Locators pick an input fill and assert it is editable
+// fill the input filed with text
+
+// In Playwright Practice session group 5.getByAltText() Locators assert the image is visible
+
+// In Playwright Practice session group 6. getByTitle() Locators assert one of the elements' full text
+
+// In Playwright Practice session group 7. getByTestId() Locators assert one of the element contains part of its text
